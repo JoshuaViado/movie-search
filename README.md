1ded5af868539c343d0438739332e7e7ba0d4e48
Joshua Viado <34035630+JoshuaViado@users.noreply.github.com>
2023-08-17 19:21:00 +0100

    refactor(1251128311): rename movie-list to movie-search

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MovieList
+# MovieSearch
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.1.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'movie-list'`, () => {
+  it(`should have as title 'movie-search'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movie-list');
+    expect(app.title).toEqual('movie-search');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('movie-list app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'movie-search app is running!'
+    );
   });
 });
