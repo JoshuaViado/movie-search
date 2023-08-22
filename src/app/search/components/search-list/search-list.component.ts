c6c500e8c164e8b6c8d34a856ab64eda9d55080e
Joshua Viado <34035630+JoshuaViado@users.noreply.github.com>
2023-08-22 11:57:15 +0100

    feat(1252942870): display movie list

--- src/app/search/components/search-list/search-list.component.ts
+++ src/app/search/components/search-list/search-list.component.ts
@@ -1,15 +1,20 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IMovie } from '../../interfaces/movie.interface';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'ms-search-list',
   standalone: true,
-  imports: [CommonModule, NgFor, MatGridListModule],
+  imports: [CommonModule, MatGridListModule],
   templateUrl: './search-list.component.html',
   styleUrls: ['./search-list.component.scss'],
 })
 export class SearchListComponent {
   @Input() movieList: IMovie[] | null = [];
+  @Output() selectMovieEvent = new EventEmitter<IMovie>();
+
+  selectMovie(movie: IMovie) {
+    this.selectMovieEvent.emit(movie);
+  }
 }
